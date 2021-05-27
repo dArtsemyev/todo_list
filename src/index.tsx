b88380c368fb96bb83721f59f318ddcec2fb5de4
dArtsemyev <d.artsemyev@gmail.com>
2021-05-27 22:01:32 +0300

    Restarted todoList app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from "./App";
 import reportWebVitals from './reportWebVitals';
-import {store} from './state/store';
-import {Provider} from 'react-redux';
-import AppWithRedux from "./AppWithRedux";
-//import AppWithReducer from "./AppWithReducer";
-//import App from "./App";
 
-ReactDOM.render(
-    <Provider store={store}>
-        {/*<App />*/}
-        {/*<AppWithReducer />*/}
-        <AppWithRedux />
-    </Provider>, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
