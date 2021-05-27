7a2df07aaa83bcdfc8ead79e847fc2acdaa2c2c2
dArtsemyev <d.artsemyev@gmail.com>
2021-05-28 00:08:11 +0300

    Created new moddule TodoList. Moved props to Todolist. Rendered Todolists with some properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from "react";
 import "./App.css";
+import TodoList from "./TodoList";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean | undefined
+}
 
 function App() {
+
+    // BLL:
+    const tasksToLearn: Array<TaskType> = [
+        {id: 1, title: "HTML/CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+    ]
+    const tasksToBuy: Array<TaskType> = [
+        {id: 1, title: "Milk", isDone: true},
+        {id: 2, title: "Beer", isDone: false},
+        {id: 3, title: "Meat", isDone: false},
+    ]
+
+    // UI:
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/><span>HTML/CSS</span></li>
-                    <li><input type="checkbox" checked={true}/><span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/><span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={"What to learn"} tasks={tasksToLearn}/>
+            <TodoList title={"What to buy"} tasks={tasksToBuy}/>
         </div>
     );
 }
